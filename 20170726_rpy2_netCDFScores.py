0cedf7819a3c0cb46af42f022a163caa38335ee6
cagau <manuelklar@mailbox.org>
2017-07-31 14:53:58 +0200

    Add files via upload

--- 20170726_rpy2_netCDFScores.py
+++ 20170726_rpy2_netCDFScores.py
@@ -13,6 +13,7 @@ Created on Tue Jul 25 16:19:47 2017
 """
 import xarray as xr
 import numpy as np
+import time
 import rpy2
 import rpy2.robjects.numpy2ri as np2ri
 from rpy2.robjects.packages import importr
@@ -35,6 +36,30 @@ def es_sample(y, dat):
         
     return srl.es_sample(y_r, dat_r)[0]
     
+def es_sample_vec(y_vec, dat_mat):
+    try:
+        y_vec = np.array(y_vec)
+        y_vec = np.expand_dims(y_vec, 1)
+        dat_mat = np.array(dat_mat)
+       # y_r = rpy2.robjects.FloatVector(y_vec)
+       # dat_r = np2ri.py2ri(dat_mat)
+    except:
+        print('Input has wrong format.')
+    else:
+        if ( 
+             len(y_vec.shape) != 3 or 
+             len(dat_mat.shape) != 3 or 
+             y_vec.shape[0] != dat_mat.shape[0] or
+             y_vec.shape[2] != dat_mat.shape[2]
+        ):
+            raise ValueError('Parameters have wrong dimension.')
+
+    df = np.concatenate((y_vec,dat_mat),axis = 1)
+    df_r = np2ri.py2ri(df)
+    rpy2.robjects.globalenv['df'] =  df_r
+    
+    return rpy2.robjects.r('mean(apply(df, c(3), function(x) es_sample(x[,1], x[,-1])))')[0]
+    
 def vs_sample(y, dat):
     try:
         y = np.array(y)
@@ -43,8 +68,32 @@ def vs_sample(y, dat):
         dat_r = np2ri.py2ri(dat)
     except:
         print('Input has wrong format.')
-        
+
     return srl.vs_sample(y_r, dat_r)[0]
+    
+def vs_sample_vec(y_vec, dat_mat):
+    try:
+        y_vec = np.array(y_vec)
+        y_vec = np.expand_dims(y_vec, 1)
+        dat_mat = np.array(dat_mat)
+       # y_r = rpy2.robjects.FloatVector(y_vec)
+       # dat_r = np2ri.py2ri(dat_mat)
+    except:
+        print('Input has wrong format.')
+    else:
+        if ( 
+             len(y_vec.shape) != 3 or 
+             len(dat_mat.shape) != 3 or 
+             y_vec.shape[0] != dat_mat.shape[0] or
+             y_vec.shape[2] != dat_mat.shape[2]
+        ):
+            raise ValueError('Parameters have wrong dimension.')
+
+    df = np.concatenate((y_vec,dat_mat),axis = 1)
+    df_r = np2ri.py2ri(df)
+    rpy2.robjects.globalenv['df'] =  df_r
+    
+    return rpy2.robjects.r('mean(apply(df, c(3), function(x) vs_sample(x[,1], x[,-1])))')[0]
     
 def crps_sample(y, dat):
     try:
@@ -89,13 +138,14 @@ forc_sb = forc[dict(time=[4])]
 obs_arr = obs_sb.to_array()
 # - drop the dimensions time, longitude and latitude and extract the numpy array
 obs_vec = obs_arr.stack(m = ('time', 'longitude', 'latitude')).values
-obs_vec_r = rpy2.robjects.FloatVector(obs_vec[0,:])
+#obs_vec_r = rpy2.robjects.FloatVector(obs_vec[0,:])
 
 forc_arr = forc_sb.to_array()
 forc_vec = forc_arr.stack(m = ('time', 'longitude', 'latitude')).values
-forc_vec_r =  np2ri.py2ri(forc_vec[0,:,:])
+#forc_vec_r =  np2ri.py2ri(forc_vec[0,:,:])
 
-# Compute Scores in Python
+# Compute Scores, in a loop
+loop_start = time.time()
 N = obs_vec.shape[1]
 crps = 0
 escr = 0
@@ -112,14 +162,28 @@ for i in range(0,N):
 crps = crps/N
 vscr = vscr/N
 escr = escr/N
+loop_end = time.time()
 print('CRPS')
 print(crps)
 print('Energy Score')
 print(escr)
 print('Variogram Score')
 print(vscr)
+print('Time needed [s]')
+print(round(loop_end - loop_start,4))
 
-# Compute Scores in R via apply
+# Compute Scores, no loop via apply (faster)
+vec_start = time.time()
 crps_v = crps_sample_vec(obs_vec[0,:], forc_vec[0,:,:])
+escr_v = es_sample_vec(obs_vec, forc_vec)
+vscr_v = vs_sample_vec(obs_vec, forc_vec)
+vec_end = time.time()
+
 print('CRPS')
 print(crps_v)
+print('Energy Score')
+print(escr_v)
+print('Variogram Score')
+print(vscr_v)
+print('Time needed [s]')
+print(round(vec_end - vec_start,4))
