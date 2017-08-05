0b8ab47b47900e169b96c1575e15a27e1066af1d
cagau <manuelklar@mailbox.org>
2017-08-05 11:53:27 +0200

    Add files via upload

--- 20170726_rpy2_netCDFScores.py
+++ 20170726_rpy2_netCDFScores.py
@@ -11,112 +11,15 @@ Created on Tue Jul 25 16:19:47 2017
 
 @author: manuel
 """
+import scoringtools as st
 import xarray as xr
 import numpy as np
 import time
-import rpy2
-import rpy2.robjects.numpy2ri as np2ri
-from rpy2.robjects.packages import importr
-srl = importr('scoringRules')
 
-ob_file = "/home/sebastian/Dropbox/Manuel_HIWI/code/ECMWF_20131001-31date_06int_uv10m_wind_MSLP_ECMWF_analysis.nc"
-fc_file = "/home/sebastian/Dropbox/Manuel_HIWI/code/EPS_uv10m_MSLP_6h_5day_20131001.nc"
-
-# Define Wrapper
-def es_sample(y, dat):
-    # Names like srl, np2ri have to be set by the ensembletools package
-    # in order to avoid imports in each function call
-    try:
-        y = np.array(y)
-        dat = np.array(dat)
-        y_r = rpy2.robjects.FloatVector(y)
-        dat_r = np2ri.py2ri(dat)
-    except:
-        print('Input has wrong format.')
-        
-    return srl.es_sample(y_r, dat_r)[0]
-    
-def es_sample_vec(y_mat, dat_mat):
-    try:
-        y_mat = np.array(y_mat)
-        y_mat = np.expand_dims(y_mat, 1)
-        dat_mat = np.array(dat_mat)
-    except:
-        print('Input has wrong format.')
-    else:
-        if ( 
-             len(y_mat.shape) != 3 or 
-             len(dat_mat.shape) != 3 or 
-             y_mat.shape[0] != dat_mat.shape[0] or
-             y_mat.shape[2] != dat_mat.shape[2]
-        ):
-            raise ValueError('Parameters have wrong dimension.')
-
-    df = np.concatenate((y_mat,dat_mat),axis = 1)
-    df_r = np2ri.py2ri(df)
-    rpy2.robjects.globalenv['df'] =  df_r
-    
-    return rpy2.robjects.r('mean(apply(df, c(3), function(x) es_sample(x[,1], x[,-1])))')[0]
-    
-def vs_sample(y, dat):
-    try:
-        y = np.array(y)
-        dat = np.array(dat)
-        y_r = rpy2.robjects.FloatVector(y)
-        dat_r = np2ri.py2ri(dat)
-    except:
-        print('Input has wrong format.')
-
-    return srl.vs_sample(y_r, dat_r)[0]
-    
-def vs_sample_vec(y_mat, dat_mat):
-    try:
-        y_mat = np.array(y_mat)
-        y_mat = np.expand_dims(y_mat, 1)
-        dat_mat = np.array(dat_mat)
-    except:
-        print('Input has wrong format.')
-    else:
-        if ( 
-             len(y_mat.shape) != 3 or 
-             len(dat_mat.shape) != 3 or 
-             y_mat.shape[0] != dat_mat.shape[0] or
-             y_mat.shape[2] != dat_mat.shape[2]
-        ):
-            raise ValueError('Parameters have wrong dimension.')
-
-    df = np.concatenate((y_mat,dat_mat),axis = 1)
-    df_r = np2ri.py2ri(df)
-    rpy2.robjects.globalenv['df'] =  df_r
-    
-    return rpy2.robjects.r('mean(apply(df, c(3), function(x) vs_sample(x[,1], x[,-1])))')[0]
-    
-def crps_sample(y, dat):
-    try:
-        y_r = float(y)
-        dat = np.array(dat)
-        dat_r = rpy2.robjects.FloatVector(dat)
-    except:
-        print('Input has wrong format.')
-        
-    return srl.crps_sample(y_r, dat_r)[0]
-    
-def crps_sample_vec(y_vec, dat_mat):
-    try:
-        y_vec = np.array(y_vec)
-        dat_mat = np.array(dat_mat)
-        y_r = rpy2.robjects.FloatVector(y_vec)
-        dat_r = np2ri.py2ri(dat_mat)
-    except:
-        print('Input has wrong format.')
-    else:
-        if len(y_vec.shape) != 1 or len(dat_mat.shape) != 2 or y_vec.shape[0] != dat_mat.shape[1]:
-            raise ValueError('Parameters have wrong dimension.')
-
-    rpy2.robjects.globalenv['obs'] =  y_r
-    rpy2.robjects.globalenv['forc'] =  dat_r
-    
-    return rpy2.robjects.r('mean(apply(rbind(obs,forc), 2, function(x) crps_sample(x[1], x[-1])))')[0]
+ob_file = "/home/manuel/Dokumente/Ausbildung_Studium/Karlsruhe/1516WS/Praktikum/ensembleScore/Input/Observation/ECMWF_20131001-31date_06int_uv10m_wind_MSLP_ECMWF_analysis.nc"
+fc_file = "/home/manuel/Dokumente/Ausbildung_Studium/Karlsruhe/1516WS/Praktikum/ensembleScore/Input/Forecast/ECMWF_okt2013/EPS_uv10m_MSLP_6h_5day_20131001.nc"
+#ob_file = "/home/sebastian/Dropbox/Manuel_HIWI/code/ECMWF_20131001-31date_06int_uv10m_wind_MSLP_ECMWF_analysis.nc"
+#fc_file = "/home/sebastian/Dropbox/Manuel_HIWI/code/EPS_uv10m_MSLP_6h_5day_20131001.nc"
     
 # Read File
 obs = xr.open_dataset(ob_file)
@@ -147,12 +50,12 @@ vscr = 0
 for i in range(0,N):
     y = obs_vec[0,i] # crps computed only for mslp    
     dat = forc_vec[0,:,i]
-    crps += crps_sample(y, dat)
+    crps += st.crps_sample(y, dat)
     
     y_vec = obs_vec[:,i]
     dat_mat = forc_vec[:,:,i]
-    escr += es_sample(y_vec, dat_mat)
-    vscr += vs_sample(y_vec, dat_mat)
+    escr += st.es_sample(y_vec, dat_mat)
+    vscr += st.vs_sample(y_vec, dat_mat)
 crps = crps/N
 vscr = vscr/N
 escr = escr/N
@@ -173,23 +76,23 @@ for i in range(0,N):
     for j in range(0,obs_vec.shape[0]):
         y = obs_vec[j,i]
         dat = forc_vec[j,:,i]
-        crps2[j,i] = crps_sample(y, dat)
+        crps2[j,i] = st.crps_sample(y, dat)
 
 # compute column-wise mean        
 np.mean(crps2, axis=1)
 
 # Compute Scores, no loop via apply (faster)
 vec_start = time.time()
-crps_v = crps_sample_vec(obs_vec[0,:], forc_vec[0,:,:]) # crps computed only for mslp 
-escr_v = es_sample_vec(obs_vec, forc_vec)
-vscr_v = vs_sample_vec(obs_vec, forc_vec)
+crps_v = st.crps_sample_vec(obs_vec[0,:], forc_vec[0,:,:]) # crps computed only for mslp 
+escr_v = st.es_sample_vec(obs_vec, forc_vec)
+vscr_v = st.vs_sample_vec(obs_vec, forc_vec)
 vec_end = time.time()
 
 print('CRPS')
-print(crps_v)
+print(crps_v.mean())
 print('Energy Score')
-print(escr_v)
+print(escr_v.mean())
 print('Variogram Score')
-print(vscr_v)
+print(vscr_v.mean())
 print('Time needed [s]')
 print(round(vec_end - vec_start,4))
